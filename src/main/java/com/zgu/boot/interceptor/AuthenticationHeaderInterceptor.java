8ce066403313262f9bf03ea17c2e4b578b8dfbfb
zhuguolong <zhuguolongi@outlook.com>
2021-09-28 21:52:25 +0800

    拦截器判断用户是否合法

--- src/main/java/com/zgu/boot/interceptor/AuthenticationHeaderInterceptor.java
+++ src/main/java/com/zgu/boot/interceptor/AuthenticationHeaderInterceptor.java
@@ -1,6 +1,9 @@
 package com.zgu.boot.interceptor;
 
+import com.alibaba.fastjson.JSONObject;
+import com.zgu.boot.common.CommonConstants;
 import com.zgu.boot.utils.RequestUtil;
+import com.zgu.boot.utils.ResultWrap;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +14,8 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import static org.springframework.util.StringUtils.hasText;
 
@@ -23,6 +28,14 @@ public class AuthenticationHeaderInterceptor implements HandlerInterceptor {
         resolveToken(request);
         if (null == RequestUtil.getUserId()) {
             response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setCharacterEncoding(CommonConstants.UTF_8);
+            response.setContentType("application/json; charset=utf-8");
+            try (PrintWriter out = response.getWriter()) {
+                String res = JSONObject.toJSON(ResultWrap.error(LOG, HttpStatus.UNAUTHORIZED.value(), "未认证！", null)).toString();
+                out.append(res);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return false;
         }
         return true;
--- src/main/java/com/zgu/boot/utils/ResultWrap.java
+++ src/main/java/com/zgu/boot/utils/ResultWrap.java
@@ -2,8 +2,9 @@ package com.zgu.boot.utils;
 
 import com.google.common.collect.ImmutableMap;
 import com.zgu.boot.common.CommonConstants;
-import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class ResultWrap {
@@ -27,11 +28,95 @@ public class ResultWrap {
      * @param result 返回的结果。
      * @return 返回一个Map表示正确返回给定的消息和数据结果。
      */
-    public static Map<String, Object> ok(@NotNull String message, @NotNull Object result) {
+    public static Map<String, Object> ok(String message, Object result) {
         return ImmutableMap.of(
                 CommonConstants.RESP_CODE, CommonConstants.OK,
                 CommonConstants.RESP_MESSAGE, message,
                 CommonConstants.RESULT, result
         );
+    }
+
+    public static Map<String, String> info(Logger logger, String respCode, String respMsg) {
+        return info(logger, respCode, respMsg, null);
+    }
+
+
+    public static Map<String, String> info(Logger logger, String respCode, String respMsg, String result) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put(CommonConstants.RESULT, result);
+        map.put(CommonConstants.RESP_CODE, respCode);
+        map.put(CommonConstants.RESP_MESSAGE, respMsg);
+
+        logger.info("{}", map);
+        return map;
+    }
+
+
+    public static Map<String, Object> info(Logger logger, String respCode, String respMsg, Object result) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put(CommonConstants.RESULT, result);
+        map.put(CommonConstants.RESP_CODE, respCode);
+        map.put(CommonConstants.RESP_MESSAGE, respMsg);
+
+        logger.info("{}", map);
+        return map;
+    }
+
+
+    public static Map<String, String> warn(Logger logger, String respCode, String respMsg) {
+        return warn(logger, respCode, respMsg, null);
+    }
+
+
+    public static Map<String, String> warn(Logger logger, String respCode, String respMsg, String result) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put(CommonConstants.RESULT, result);
+        map.put(CommonConstants.RESP_CODE, respCode);
+        map.put(CommonConstants.RESP_MESSAGE, respMsg);
+
+        logger.warn("{}", map);
+        return map;
+    }
+
+
+    public static Map<String, Object> warn(Logger logger, String respCode, String respMsg, Object result) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put(CommonConstants.RESULT, result);
+        map.put(CommonConstants.RESP_CODE, respCode);
+        map.put(CommonConstants.RESP_MESSAGE, respMsg);
+
+        logger.warn("{}", map);
+        return map;
+    }
+
+    public static Map<String, Object> err(Logger logger, String respCode, String respMsg) {
+        return err(logger, respCode, respMsg, null);
+    }
+
+
+    public static Map<String, Object> err(Logger logger, String respCode, String respMsg, String result) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put(CommonConstants.RESULT, result);
+        map.put(CommonConstants.RESP_CODE, respCode);
+        map.put(CommonConstants.RESP_MESSAGE, respMsg);
+
+        logger.error("{}", map);
+        return map;
+    }
+
+    public static Map<String, Object> error(Logger logger, int respCode, String respMsg, Object result) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put(CommonConstants.RESP_CODE, respCode);
+        map.put(CommonConstants.RESP_MESSAGE, respMsg);
+        map.put(CommonConstants.RESULT, result);
+
+        logger.error("{}", map);
+        return map;
     }
 }
