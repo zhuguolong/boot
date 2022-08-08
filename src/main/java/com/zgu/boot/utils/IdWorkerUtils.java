9d2a59b048f0123963194ecaa13dddc065c81c7c
zhuguolong <zhuguolongi@outlook.com>
2022-08-08 15:35:42 +0800

    雪花算法工具类 IdWorkerUtils

---
+++ src/main/java/com/zgu/boot/utils/IdWorkerUtils.java
@@ -0,0 +1,172 @@
+package com.zgu.boot.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 雪花算法解析 结构 snowflake的结构如下(每部分用-分开):
+ * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ * 第一位为未使用，接下来的41位为毫秒级时间(41位的长度可以使用69年)，然后是5位datacenterId和5位workerId(10
+ * 位的长度最多支持部署1024个节点） ，最后12位是毫秒内的计数（12位的计数顺序号支持每个节点每毫秒产生4096个ID序号）
+ * <p>
+ * 一共加起来刚好64位，为一个Long型。(转换成字符串长度为18)
+ */
+public class IdWorkerUtils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IdWorkerUtils.class);
+
+    /**
+     * 工作机器ID(0~31)
+     */
+    private long workerId;
+
+    /**
+     * 数据中心ID(0~31)
+     */
+    private long datacenterId;
+
+    /**
+     * 毫秒内序列(0~4095)
+     */
+    private long sequence;
+
+    /**
+     * 开始时间截 (2015-01-01)
+     */
+    private long twepoch = 1288834974657L;
+
+    /**
+     * 机器id所占的位数
+     */
+    private long workerIdBits = 5L;
+
+    /**
+     * 数据标识id所占的位数
+     */
+    private long datacenterIdBits = 5L;
+
+    /**
+     * 这个是二进制运算，就是 5 bit最多只能有31个数字，也就是说机器id最多只能是32以内
+     */
+    private long maxWorkerId = -1L ^ (-1L << workerIdBits);
+
+    /**
+     * 这个是二进制运算，就是 5 bit最多只能有31个数字，机房id最多只能是32以内
+     */
+    private long maxDatacenterId = -1L ^ (-1L << datacenterIdBits);
+
+    /**
+     * 序列在id中占的位数
+     */
+    private long sequenceBits = 12L;
+
+    /**
+     * 机器ID向左移12位
+     */
+    private long workerIdShift = sequenceBits;
+
+    /**
+     * 数据标识id向左移17位(12+5)
+     */
+    private long datacenterIdShift = sequenceBits + workerIdBits;
+
+    /**
+     * 时间截向左移22位(5+5+12)
+     */
+    private long timestampLeftShift = sequenceBits + workerIdBits + datacenterIdBits;
+
+    /**
+     * 生成序列的掩码，这里为4095 (0b111111111111=0xfff=4095)
+     */
+    private long sequenceMask = -1L ^ (-1L << sequenceBits);
+
+    /**
+     * 上次生成ID的时间截
+     */
+    private long lastTimestamp = -1L;
+
+    public IdWorkerUtils(long workerId, long datacenterId, long sequence) {
+        // sanity check for workerId
+        // 这儿不就检查了一下，要求就是你传递进来的机房id和机器id不能超过32，不能小于0
+        if (workerId > maxWorkerId || workerId < 0) {
+            throw new IllegalArgumentException(
+                    String.format("worker Id can't be greater than %d or less than 0", maxWorkerId));
+        }
+        if (datacenterId > maxDatacenterId || datacenterId < 0) {
+            throw new IllegalArgumentException(
+                    String.format("datacenter Id can't be greater than %d or less than 0", maxDatacenterId));
+        }
+
+        LOGGER.info(
+                "worker starting. timestamp left shift {}, datacenter id bits {}, worker id bits {}, sequence bits {}, workerid {}",
+                timestampLeftShift, datacenterIdBits, workerIdBits, sequenceBits, workerId);
+
+        this.workerId = workerId;
+        this.datacenterId = datacenterId;
+        this.sequence = sequence;
+    }
+
+
+    public long getWorkerId() {
+        return workerId;
+    }
+
+    public long getDatacenterId() {
+        return datacenterId;
+    }
+
+    public long getTimestamp() {
+        return System.currentTimeMillis();
+    }
+
+    public synchronized long nextId() {
+        // 这儿就是获取当前时间戳，单位是毫秒
+        long timestamp = timeGen();
+
+        if (timestamp < lastTimestamp) {
+            LOGGER.error("clock is moving backwards.  Rejecting requests until {}.", lastTimestamp);
+            throw new RuntimeException(String.format(
+                    "Clock moved backwards.  Refusing to generate id for %d milliseconds", lastTimestamp - timestamp));
+        }
+
+        if (lastTimestamp == timestamp) {
+            // 这个意思是说一个毫秒内最多只能有4096个数字
+            // 无论你传递多少进来，这个位运算保证始终就是在4096这个范围内，避免你自己传递个sequence超过了4096这个范围
+            sequence = (sequence + 1) & sequenceMask;
+            if (sequence == 0) {
+                timestamp = tilNextMillis(lastTimestamp);
+            }
+        } else {
+            sequence = 0;
+        }
+
+        // 这儿记录一下最近一次生成id的时间戳，单位是毫秒
+        lastTimestamp = timestamp;
+
+        // 这儿就是将时间戳左移，放到 41 bit那儿；
+        // 将机房 id左移放到 5 bit那儿；
+        // 将机器id左移放到5 bit那儿；将序号放最后12 bit；
+        // 最后拼接起来成一个 64 bit的二进制数字，转换成 10 进制就是个 long 型
+        return ((timestamp - twepoch) << timestampLeftShift) | (datacenterId << datacenterIdShift)
+                | (workerId << workerIdShift) | sequence;
+    }
+
+    private long tilNextMillis(long lastTimestamp) {
+        long timestamp = timeGen();
+        while (timestamp <= lastTimestamp) {
+            timestamp = timeGen();
+        }
+        return timestamp;
+    }
+
+    private long timeGen() {
+        return System.currentTimeMillis();
+    }
+
+    // ---------------测试---------------
+//    public static void main(String[] args) {
+//        IdWorkerUtils worker = new IdWorkerUtils(1, 1, 1);
+//        for (int i = 0; i < 30; i++) {
+//            System.out.println(worker.nextId());
+//        }
+//    }
+}
